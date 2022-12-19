47468dde40af295a6495b7a922cc8cc4e6591156
viniciuscosta126 <viniciuscosta126@gmail.com>
2022-12-19 10:26:56 -0300

    Validacao de cidades

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, RequestHandler, Response } from "express";
 import * as yup from "yup";
 import { StatusCodes } from "http-status-codes";
 interface ICidade {
@@ -10,12 +10,12 @@ const bodyValidation: yup.SchemaOf<ICidade> = yup.object().shape({
     estado: yup.string().required().min(3),
 });
 
-export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
-    let validateData: ICidade | undefined = undefined;
+export const createBodyValidator: RequestHandler = async (req, res, next) => {
     try {
-        validateData = await bodyValidation.validate(req.body, {
+        await bodyValidation.validate(req.body, {
             abortEarly: false,
         });
+        return next();
     } catch (error) {
         const yupError = error as yup.ValidationError;
 
@@ -25,10 +25,11 @@ export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
             if (!error.path) return;
             errors[error.path] = error.message;
         });
-        return res.status(StatusCodes.BAD_REQUEST).json({
-            errors,
-        });
+        return res.status(StatusCodes.BAD_REQUEST).json({ errors });
     }
+};
 
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+    console.log(req.body);
     return res.send("Create!");
 };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router, Request, Response } from "express";
-import {StatusCodes} from 'http-status-codes'
-import {CidadesController} from '../controllers'
+import { StatusCodes } from "http-status-codes";
+import { CidadesController } from "../controllers";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
     return res.status(StatusCodes.OK).send("Servidor funcionando");
 });
 
-router.post('/cidades',CidadesController.create)
+router.post("/cidades", CidadesController.createBodyValidator,CidadesController.create);
 export { router };
